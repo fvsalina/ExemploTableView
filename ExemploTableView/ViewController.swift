98ed41a829c06ffd9758c3014f24ab35c0233528
Fernando Salina <fsalina@gmail.com>
2019-09-27 19:51:06 -0300

    Initial Commit

---
+++ ExemploTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExemploTableView
+//
+//  Created by Fernando Vernal Salina on 27/09/19.
+//  Copyright © 2019 Fernando Vernal Salina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
